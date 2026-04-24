8023d9d9d6acd81554abc9c94114ba44f73071e3
Daita Saathwika <saathwika04012005@gmail.com>
2026-04-24 18:57:43 +0530

    update frontend endpoints for heroku

--- frontend/script.js
+++ frontend/script.js
@@ -58,7 +58,7 @@ document.addEventListener('DOMContentLoaded', () => {
         submitBtn.disabled = true;
 
         try {
-            const response = await fetch('http://localhost:3000/bfhl', {
+            const response = await fetch('/bfhl', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
